fdc1ba565b9408828a242f752017c3ff649c7c28
Jeremias Leiton <jeremiasleiton@gmail.com>
2026-05-16 10:48:35 +0100

    First commit

---
+++ src/main/java/com/jleiton/incomeandcost/IncomeandcostApplication.java
@@ -0,0 +1,13 @@
+package com.jleiton.incomeandcost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomeandcostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncomeandcostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jleiton/incomeandcost/IncomeandcostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jleiton.incomeandcost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncomeandcostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
