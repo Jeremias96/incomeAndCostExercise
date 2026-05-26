b356afb57ded587cb02afa645355e11e782c1281
Jeremias Leiton <jeremiasleiton@gmail.com>
2026-05-26 22:11:40 +0100

    Add null values handling

---
+++ src/main/java/com/jleiton/incomeandcost/exception/InvalidValuesRequestException.java
@@ -0,0 +1,8 @@
+package com.jleiton.incomeandcost.exception;
+
+public class InvalidValuesRequestException extends RuntimeException {
+    
+    public InvalidValuesRequestException(String message){
+        super(message);
+    }
+}
